e189b2ee6647c2e2c7ebf20421b4e9f3d2ee8ad7
Vinod Panicker <vinod.panicker@wipro.com>
2016-02-18 17:17:38 +0530

    added COR code

--- drupal-8.0.3/index.php
+++ drupal-8.0.3/index.php
@@ -10,13 +10,30 @@
 
 use Drupal\Core\DrupalKernel;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $autoloader = require_once 'autoload.php';
+
+// Make sure OPTIONS method returns proper headers for CORS.
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  $response = new Response(200);
+  $response->headers->set('Access-Control-Allow-Origin', '*');
+  $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, DELETE');
+  $response->headers->set('Access-Control-Allow-Headers', 'Authorization, X-CSRF-Token, Content-Type, Accept');
+  $response->send();
+  exit;
+}
 
 $kernel = new DrupalKernel('prod', $autoloader);
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+
+// Enable CORS requests, Change '*' for any specific domain or ip address.
+$response->headers->set('Access-Control-Allow-Origin', '*');
+$response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, DELETE');
+$response->headers->set('Access-Control-Allow-Headers', 'Authorization, X-CSRF-Token, Content-Type, Accept');
+
 $response->send();
 
 $kernel->terminate($request, $response);
